1be22b45ca40bf8c5e0bc4b74b094b1a8c919466
Trinidad <felipe.trinidad@solera.com>
2022-11-29 10:32:36 +0100

    TDD: Login service, done test, before implementation

--- crm-backend/src/main/java/com/crm/services/UserService.java
+++ crm-backend/src/main/java/com/crm/services/UserService.java
@@ -1,8 +1,19 @@
 package com.crm.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.crm.models.User;
+import com.crm.repositories.UserRepository;
 
 @Service
 public class UserService {
-
+	
+	@Autowired
+	UserRepository Repository;
+	
+	public Boolean logIn(User x) {
+		Boolean bool = false;
+		return bool;
+	}
 }
--- crm-backend/src/test/java/com/crm/services/test/UserSeviceTest.java
+++ crm-backend/src/test/java/com/crm/services/test/UserSeviceTest.java
@@ -5,7 +5,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 import com.crm.models.User;
+import com.crm.services.UserService;
 
 public class UserSeviceTest {
-
+	
+	UserService userService;
+	//Login Test, the user must Log in and get the logged token
+	@Test
+	public void logInTest(){
+		User x = new User(1, "prueba1", "prueba1");
+		Boolean y = false;
+		y=userService.logIn(x);
+		Assert.isTrue(y);
+	}
+	//Login Test, the user must NOT Log in, as it doesn't have password
+	public void logInErrorTest(){
+		User x = new User(1, "prueba1", "");
+		Boolean y = false;
+		y=userService.logIn(x);
+		Assert.isTrue(!y);
+	}
 }
